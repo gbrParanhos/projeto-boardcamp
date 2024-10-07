099d2ef72d1c90a7e916594afc7fe3fe53e2810f
gbrParanhos <gbrparanhos@gmail.com>
2024-10-07 17:35:23 -0300

    initial commit

---
+++ src/config/connection.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import pg from "pg";
+
+const { Pool } = pg;
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+export const db = new Pool(configDatabase);
